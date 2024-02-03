131f16d21452d78245cd5da83b7bf83499a623b2
Ramitphi <18ucs144@lnmiit.ac.in>
2024-02-03 16:07:58 +0530

    fixed public url

--- src/app/api/frame/route.ts
+++ src/app/api/frame/route.ts
@@ -5,7 +5,7 @@ import {
 } from "@coinbase/onchainkit";
 import { NextRequest, NextResponse } from "next/server";
 
-const NEXT_PUBLIC_URL = "https://frames-new.vercel.app/";
+const NEXT_PUBLIC_URL = "https://frames-new.vercel.app";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = "";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { getFrameMetadata } from "@coinbase/onchainkit";
 import type { Metadata } from "next";
 
-const NEXT_PUBLIC_URL = "https://frames-new.vercel.app/";
+const NEXT_PUBLIC_URL = "https://frames-new.vercel.app";
 
 const frameMetadata = getFrameMetadata({
   buttons: [
