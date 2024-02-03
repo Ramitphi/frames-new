e592dff2d884d1e1f709974d134a78559f2c48cc
Ramitphi <18ucs144@lnmiit.ac.in>
2024-02-03 16:00:23 +0530

    added basic metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,33 @@
-import Image from "next/image";
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import type { Metadata } from "next";
+
+const NEXT_PUBLIC_URL = "https://frames-new.vercel.app/";
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Click Me",
+    },
+  ],
+  image: `${NEXT_PUBLIC_URL}/park-1.png`,
+  input: {
+    text: "Tell me a boat story",
+  },
+  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: "ramit.xyz",
+  description: "LFG",
+  openGraph: {
+    title: "ramit",
+    description: "LFG",
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
 
 export default function Home() {
   return <>ramit</>;
